91ce64f3513d197995756a483455e781adce40fa
SajidKawnsar <sajidkaunsar@gmail.com>
2016-08-16 17:40:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 var http = require('http');
-var url = require('url');
-var querystring = require('querystring')
+/**var url = require('url');
+var querystring = require('querystring')**/
 var server = http.createServer(function(request,response){
 data1="";
-request.on('data',function(chunk){data1+=chunk;});
+/**request.on('data',function(chunk){data1+=chunk;});
 request.on('end',function(){
 var qs = querystring.parse(data1);
 var num1 = parseInt(qs['num1']);
 var num2 = parseInt(qs['num2']);
-var result= num1+num2;
+var result= num1+num2;**/
 response.writeHead(200,{"Content-Type":"text/html"});
-response.write("Addition of " + " " +num1+ " " + "&"+" " +num2+ " " + "=" + " "+result); 
+response.write("Welcome to first node app..."); 
 response.end();
 });
 });
