321f8754e17be06645c496fb6ea3420a6ff816ee
SajidKawnsar <sajidkaunsar@gmail.com>
2016-08-18 11:43:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 var http = require('http');
 var url = require('url');
+var server = http.createServer(create);
 var querystring = require('querystring')
-var server = http.createServer(function(request,response){
-//data1="";
-//request.on('data',function(chunk){data1+=chunk;});
-//request.on('end',function(){
-//var qs = querystring.parse(data1);
-//var num1 = parseInt(qs['num1']);
-//var num2 = parseInt(qs['num2']);
-//var result= num1+num2;
-response.writeHead(200,{"Content-Type":"text/html"});
-response.write("Welcome to NODE.JS" ); 
-response.end();
+function create(request,response){
+data1="";
+data2="";
+request.on('data',function(chunk){data1+=chunk;});
+request.on('end',function(){
+var qs = querystring.parse(data1);
+console.log(data1);
+var num1 = parseInt(qs['num1']);
+var num2 = parseInt(qs['num2']);
+var options = {
+  host: '10.152.167.241',
+  port: 9010,
+  path: '/MyController/'+num1+'/'+num2
+};
+http.get(options, function(resp){
+  resp.setEncoding('utf8');
+  resp.on('data', function(chunk){
+  response.writeHead(200,{"Content-Type":"text/html"});
+  response.write("Addition of "+num1+" "+"&"+" "+num2+" " +"=" +" "+chunk); 
+  response.end();
+  });
+}).on("error", function(e){
+  console.log("Got error: " + e.message);
 });
-//});
-server.listen(3333);
-console.log("Server running at port 3333...");
+
+});
+}
+server.listen(4444);
+console.log("server listening to 4444");
