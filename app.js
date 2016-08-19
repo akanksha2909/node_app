d4776d1339ecbdc6fac2e4f4823050338973408e
SajidKawnsar <sajidkaunsar@gmail.com>
2016-08-19 11:52:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
 var http = require('http');
 var url = require('url');
 var server = http.createServer(create);
-var querystring = require('querystring')
+var querystring = require('querystring');
+var reqPlay = require('./requestPlay.js');
+var route = require('./router.js');
+var fs = require('fs');
 function create(request,response){
-data1="";
-data2="";
-request.on('data',function(chunk){data1+=chunk;});
-request.on('end',function(){
-var qs = querystring.parse(data1);
-console.log(data1);
-var num1 = parseInt(qs['num1']);
-var num2 = parseInt(qs['num2']);
-var options = {
-  host: '172.17.0.2',
-  port: 9000,
-  path: '/MyController/'+num1+'/'+num2
-};
-http.get(options, function(resp){
-  resp.setEncoding('utf8');
-  resp.on('data', function(chunk){
-  response.writeHead(200,{"Content-Type":"text/html"});
-  response.write("Addition of "+num1+" "+"&"+" "+num2+" " +"=" +" "+chunk); 
-  response.end();
-  });
-}).on("error", function(e){
-  console.log("Got error: " + e.message);
-});
+if(request.url === "/" || request.url === "/calculator.html"){
+    fs.readFile("./calculator.html",'utf8', function (error,data) {
+            if (error) {
+                response.writeHead(404);
+                response.write("<html><head><title>Error</title></head><body bgcolor='#E2C2F6'><br><center><h3 style='color:red;'>The requested content is not available...</h3></center></body></html>");
+				response.end();	
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html" });
+                response.write(data);
+				response.end();
+            }
+    });         
+	
+}
+else{
+	response.writeHead(404);
+    response.write("<html><head><title>Error</title></head><body bgcolor='#E2C2F6'><br><center><h3 style='color:red;'>The webpage you are looking for is not available...</h3></center></body></html>");
+	response.end();	
+}
 
-});
 }
 server.listen(4444);
 console.log("server listening to 4444");
