c7c39c9d7b6cb698e5eefde36add2e7434a20806
SajidKawnsar <sajidkaunsar@gmail.com>
2016-08-19 11:37:56 +0530

    Delete module11.js

--- module11.js
+++
@@ -1,6 +0,0 @@
-exports.myFunc =function(theUrl){   console.log("in request module");
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET",url, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-};
